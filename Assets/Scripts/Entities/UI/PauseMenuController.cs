ca6cf7fc249c41204eca7e523579859cb0c42c47
peabnuts123 <peabnuts123@gmail.com>
2019-07-14 22:13:36 +1200

    Add pause menu to game + ability to quit

---
+++ Assets/Scripts/Entities/UI/PauseMenuController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+
+    // Public references
+    [NotNull]
+    public GameObject pauseMenuObject;
+
+    public void ShowPauseMenu()
+    {
+        this.pauseMenuObject.SetActive(true);
+    }
+
+    public void HidePauseMenu()
+    {
+        this.pauseMenuObject.SetActive(false);
+    }
+
+    public void TogglePauseMenu()
+    {
+        this.pauseMenuObject.SetActive(!this.pauseMenuObject.activeSelf);
+    }
+
+    public void ExitApplication()
+    {
+#if UNITY_EDITOR 
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            this.TogglePauseMenu();
+        }
+    }
+
+
+}
